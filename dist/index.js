999a1b1aa9f0aadea11cb1ea8602ab03d7b9b261
wtzeng <wentao_zeng1@163.com>
2018-07-16 11:01:57 +0800

    optimize query function

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const API_URL = 'http://www.stands4.com/services/v2/abbr.php';
 const UID = 'uid';
 const TOKEN_ID = 'token_id';
 
-function genQueryURL(term, searchType = 'e', sortby = 'p') {
+function genQueryURL(term, searchType, sortby) {
   const params = {
     uid: UID,
     tokenid: TOKEN_ID,
@@ -29,20 +29,17 @@ function formatResponse(data) {
   }))
 }
 
-function query(term, searchType, sortby) {
-  const url = genQueryURL(term, searchType, sortby);
+const defaultResponse = [{title: 'No result...', subtitle: 'Abbreviation', arg: 0}];
 
+function query(input, sortby = 'p') {
+  const args = input.split(' ');
+  const isReverse = args[0] === 'r' && args.length > 1;
+  const term = isReverse ? args[1] : args[0];
+  const searchType = isReverse ? 'r' : 'e';
+  const url = genQueryURL(term, searchType, sortby);
   return alfy.fetch(url, {json: false}).then((responseXML) => {
     const responseJson = fastXMLParser.parse(responseXML);
-    if (responseJson.results.result) {
-      return formatResponse(responseJson.results.result)
-    } else {
-      return [{
-        title: 'No result...',
-        subtitle: 'Abbreviation...',
-        arg: 0,
-      }]
-    }
+    return responseJson.results.result ? formatResponse(responseJson.results.result) : defaultResponse
   })
 }
 
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import fastXMLParser from 'fast-xml-parser'
 import alfy from 'alfy'
 import { API_URL, UID, TOKEN_ID } from './config'
 
-function genQueryURL(term, searchType = 'e', sortby = 'p') {
+function genQueryURL(term, searchType, sortby) {
   const params = {
     uid: UID,
     tokenid: TOKEN_ID,
@@ -22,19 +22,16 @@ function formatResponse(data) {
   }))
 }
 
-export function query(term, searchType, sortby) {
-  const url = genQueryURL(term, searchType, sortby)
+const defaultResponse = [{title: 'No result...', subtitle: 'Abbreviation', arg: 0}]
 
+export function query(input, sortby = 'p') {
+  const args = input.split(' ')
+  const isReverse = args[0] === 'r' && args.length > 1
+  const term = isReverse ? args[1] : args[0]
+  const searchType = isReverse ? 'r' : 'e'
+  const url = genQueryURL(term, searchType, sortby)
   return alfy.fetch(url, {json: false}).then((responseXML) => {
     const responseJson = fastXMLParser.parse(responseXML)
-    if (responseJson.results.result) {
-      return formatResponse(responseJson.results.result)
-    } else {
-      return [{
-        title: 'No result...',
-        subtitle: 'Abbreviation...',
-        arg: 0,
-      }]
-    }
+    return responseJson.results.result ? formatResponse(responseJson.results.result) : defaultResponse
   })
 }
